35058d331011fbe4b997fe27fa4e962ca5109d94
guilherme <guilherme@playmoresoftware.com>
2022-01-23 19:37:43 -0300

    Service and basic findAll

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll() => _context.Seller.ToList();
+    }
+}
