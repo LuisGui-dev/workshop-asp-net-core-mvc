74d4197d08e932489fb1b547fe6e8abaf20edb91
guilherme <guilherme@playmoresoftware.com>
2022-01-24 09:52:02 -0300

    Asynchronous operations

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 
@@ -14,6 +16,11 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-        public List<Department> FindAll() => _context.Department.OrderBy(x => x.Name).ToList();
+        public async Task<List<Department>> FindAllAsync()
+        {
+            return await _context.Department
+                .OrderBy(x => x.Name)
+                .ToListAsync();
+        }
     }
 }
