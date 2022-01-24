8e8446c0f895bc22101f5831c6f3cba728934266
guilherme <guilherme@playmoresoftware.com>
2022-01-24 09:31:39 -0300

    Validations

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,12 +22,10 @@ namespace SalesWebMvc
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -41,9 +42,18 @@ namespace SalesWebMvc
             services.AddScoped<DepartmentService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUs = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUs),
+                SupportedCultures = new List<CultureInfo>{enUs},
+                SupportedUICultures = new List<CultureInfo>{enUs}
+            };
+            
+            app.UseRequestLocalization(localizationOptions);
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
